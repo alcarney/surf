3da6522a9119b6d783f4aace701dea60e51ca56d
Alex Carney <alcarneyorg@gmail.com>
2014-12-28 13:21:24 +0000

    Applied the homepage patch

--- config.def.h
+++ config.def.h
@@ -31,6 +31,7 @@ static Bool loadimages = TRUE;
 static Bool hidebackground  = FALSE;
 static Bool allowgeolocation = TRUE;
 
+#define HOMEPAGE "https://www.duckduckgo.com/"
 #define SETPROP(p, q) { \
 	.v = (char *[]){ "/bin/sh", "-c", \
 		"prop=\"`xprop -id $2 $0 | cut -d '\"' -f 2 | xargs -0 printf %b | dmenu`\" &&" \
